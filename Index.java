d5c91463d75bb8d9523d2e5252945ff6fd356b2a
maniK9842 <44579980+maniK9842@users.noreply.github.com>
2025-02-24 14:43:14 +0530

    added third printing stmt

--- Index.java
+++ Index.java
@@ -2,5 +2,6 @@ class Index {
   public static void main(String args[]) {
     System.out.println("Welcome Manikandan");
     System.out.println("Welcome Manikandan2");
+    System.out.println("Welcome Manikandan3");
   }
 }
